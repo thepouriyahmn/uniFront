a0d592e86825256e93e68640ad9de4399d96ae37
pouriya <pouriyahmn@gmail.com>
2025-07-07 19:05:33 +0330

    fix the login for ws

--- js/protocol.js
+++ js/protocol.js
@@ -1,7 +1,7 @@
 //protocol.js:
 class ProtocolAdapter {
     async signup(userData) {}
-   
+   async login(data) {}
 }
 class HTTPAdapter extends ProtocolAdapter {
     async signup(userData) {
@@ -12,6 +12,17 @@ class HTTPAdapter extends ProtocolAdapter {
         });
         return res;
     }
+    async login(data){
+  let loginRequest = await fetch("http://localhost:9001/login", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        return loginRequest
+    }
+
 }
 class WebSocketAdapter extends ProtocolAdapter {
     constructor() {
@@ -19,6 +30,10 @@ class WebSocketAdapter extends ProtocolAdapter {
         this.ws = new WebSocket("ws://localhost:9001/signupWS");
         this.ready = new Promise(resolve => {
             this.ws.onopen = () => resolve();
+        });
+          this.loginWS = new WebSocket("ws://localhost:9001/loginWS");
+        this.loginReady = new Promise(resolve => {
+            this.loginWS.onopen = () => resolve();
         });
     }
 
@@ -46,9 +61,34 @@ class WebSocketAdapter extends ProtocolAdapter {
             }));
         });
     }
+       async login(data){
+   
+    await this.loginReady;  // انتظار باز شدن کانکشن loginWS
+
+    return new Promise((resolve, reject) => {
+        this.loginWS.onmessage = (msg) => {
+            const resData = JSON.parse(msg.data);
+            resolve({
+                ok: resData.status === "ok",
+                status: resData.code, // مثل 401، 200 و غیره
+                json: async () => resData,
+            });
+            console.log("msg is: ", msg);
+        };
+
+        this.loginWS.onerror = reject;
+
+        this.loginWS.send(JSON.stringify({
+            username: data.username,
+            password: data.password
+        }));
+    });
 }
 
-let protocol = "ws"; // یا "ws"
+}
+
+
+let protocol = "ws"; 
 let adapter;
 
 if (protocol === "http") {
